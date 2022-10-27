6ecbf1e948f195a88fd55cab40b6fe7f2d4f6494
sello08 <selimakyol25@gmail.com>
2022-10-27 19:57:04 +0300

    deneme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,4 +18,4 @@ import { CalculatorComponent } from './components/calculator/calculator.componen
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }    
