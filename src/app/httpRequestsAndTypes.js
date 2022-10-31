648bcd8426b1ec787b45846f48a73d45f21bfd75
sello08 <selimakyol25@gmail.com>
2022-10-31 17:02:40 +0300

    https requests

---
+++ src/app/httpRequestsAndTypes.js
@@ -0,0 +1,16 @@
+GET
+"Belirli bir kaynaktan verileri çekebilmek için get metodu kullanılır"
+ "Özellikle istenilen veriler için fonksiyon içinde parametre verilerek gelen veri filtrelenebilir"
+ 
+ "get('http://localhost:3000/categories')"
+
+POST
+"İstenilen kaynağa veri aktarımı yapılması için kullanılabilir. "
+"Parameter olarak gönderilmek istenen veri htttp adresiyle birlikte payload içinde  gönderilir."
+
+PUT
+"Halihazırda var olan veriler payload aracılığıyla güncellenebilir."
+
+DELETE
+"Var olan verilere delete isteği atılarak ve datada var olan parametre gönderilerek silinebilir."
+
